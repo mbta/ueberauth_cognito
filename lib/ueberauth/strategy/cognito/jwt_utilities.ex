217ff5219f4eadd3d6857edfb30ff18221b14f21
Maldonado <emaldonado@mbta.com>
2019-05-20 12:55:51 -0400

    Refactor JWT verify function

--- lib/ueberauth/strategy/cognito/jwt_utilities.ex
+++ lib/ueberauth/strategy/cognito/jwt_utilities.ex
@@ -6,17 +6,22 @@ defmodule Ueberauth.Strategy.Cognito.JwtUtilities do
   @doc "Verifies that a JWT is valid: the signature is correct,
   and the audience is the AWS client_id"
   def verify(jwt, jwks, client_id) do
-    individual_jwks = Enum.map(jwks["keys"], &JOSE.JWK.from(&1))
+    with {:ok, claims_json} <- verified_claims(jwks["keys"], jwt),
+         {:ok, claims} <- Jason.decode(claims_json),
+         true <- claims["aud"] == client_id,
+         true <- claims["exp"] > System.system_time(:seconds) do
+      {:ok, claims}
+    else
+      _ ->
+        {:error, :invalid_jwt}
+    end
+  end
 
-    Enum.find_value(individual_jwks, {:error, :invalid_jwt}, fn jwk ->
-      with {true, claims_json, _} <- JOSE.JWS.verify_strict(jwk, ["RS256"], jwt),
-           {:ok, claims} <- Jason.decode(claims_json),
-           true <- claims["aud"] == client_id,
-           true <- claims["exp"] > System.system_time(:seconds) do
-        {:ok, claims}
-      else
-        _ ->
-          nil
+  defp verified_claims(jwks, jwt) do
+    Enum.find_value(jwks, fn jwk ->
+      case JOSE.JWS.verify_strict(jwk, ["RS256"], jwt) do
+        {true, claims_json, _} -> {:ok, claims_json}
+        _ -> nil
       end
     end)
   end
