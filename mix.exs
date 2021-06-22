122007db35699c809f81144aa091d5a2f1ff1d9e
Maldonado <emaldonado@mbta.com>
2021-06-22 16:18:04 -0400

    chore: upgrade to Elixir 1.10 and Erlang 22

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule UeberauthCognito.MixProject do
     [
       app: :ueberauth_cognito,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: LcovEx],
